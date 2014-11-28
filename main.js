c7919666f7ef71849ac24adb3016bcf327e00192
mookiejones <Charles.Heath.Berman@gmail.com>
2014-11-28 12:27:10 -0500

    update

--- main.js
+++
@@ -1,7 +0,0 @@
-var mapElement = document.querySelector('google-map');
-mapElement.addEventListener('google-map-ready', mapLoaded);
-
-function mapLoaded(e) {
-  var map = this.map;
-  console.log('current zoom: ' + map.getZoom());
-}
